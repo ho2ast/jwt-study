5e31623379d096af65ff8090f5313bc4aacb1430
dh.nam <dh.nam@okestro.com>
2022-08-27 17:26:33 +0900

    jwt init commit

---
+++ src/main/java/com/ho2ast/jwt/config/SecurityConfig.java
@@ -0,0 +1,39 @@
+package com.ho2ast.jwt.config;
+
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.web.filter.CorsFilter;
+
+@Configuration
+@EnableWebSecurity
+@RequiredArgsConstructor
+public class SecurityConfig {
+
+    private final CorsFilter corsFilter;
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        http.csrf().disable();
+        http.sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS)// 세션 방식을 사용하지 않음
+                .and()
+                .addFilter(corsFilter)
+                .formLogin().disable() // form 방식 로그인 안함
+                .httpBasic().disable() //
+                .authorizeRequests()
+                .antMatchers("/api/v1/user/**")
+                .access("hasRole('ROLE_USER') or hasRole('ROLE_MANAGER') or hasRole('ROLE_ADMIN')")
+                .antMatchers("/api/v1/manager/**")
+                .access("hasRole('ROLE_MANAGER') or hasRole('ROLE_ADMIN')")
+                .antMatchers("/api/v1/admin/**")
+                .access("hasRole('ROLE_ADMIN')")
+                .anyRequest().permitAll();
+
+        return http.build();
+    }
+}
---
+++ src/test/java/com/ho2ast/jwt/JwtApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ho2ast.jwt;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JwtApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
